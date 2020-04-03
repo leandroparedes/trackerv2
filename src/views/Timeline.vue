fbc079983ca4ab97993b798871156174f50bd4ec
Leandro Paredes <leandroparedes6@gmail.com>
2020-04-03 14:33:38 -0300

    highlights

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -12,20 +12,27 @@
                 v-for="(situation, index) in timeline.data"
                 :key="index"
             >
+                <template v-slot:opposite>
+                    <highlights :highlights="situation.highlights"></highlights>
+                </template>
                 <situation-card :situation="situation"></situation-card>
             </v-timeline-item>
         </v-timeline>
     </div>
+    <v-overlay v-else :value="! timeline.loaded">
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import SituationCard from '@/components/timeline/SituationCard.vue';
+import Highlights from '@/components/timeline/Highlights.vue';
 
 export default {
     name: 'Timeline',
 
-    components: { SituationCard },
+    components: { SituationCard, Highlights },
 
     mounted () {
         this.$store.dispatch('fetch_timeline_data');
