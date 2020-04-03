76b3292f0b34f5a5bca3e29cf1904b01dd415d00
Leandro Paredes <leandroparedes6@gmail.com>
2020-04-02 21:52:01 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,15 @@
                 <router-view :key="$route.fullPath"></router-view>
             </v-container>
         </v-content>
+
+        <v-footer padless class="font-weight-medium">
+            <v-col class="text-center" cols="12">
+                {{ new Date().getFullYear() }} —
+                <strong>Developed by
+                    <a href="https://github.com/leandroparedes" target="_blank">Leandro Paredes</a>
+                </strong>
+            </v-col>
+        </v-footer>
     </v-app>
 </template>
 
